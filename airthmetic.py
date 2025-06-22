e36041c000012112be8d929e7b3c93c68b44f434
Amit Tiwari <tiwari@solandeo.com>
2025-06-23 00:58:44 +0200

    clean up

--- airthmetic.py
+++ airthmetic.py
@@ -18,7 +18,3 @@ print(f"the sum of two number is: {sum}")
 mul = num_calc.mul_two_numbers(3,4)
 print(f"the mul of two number is: {mul}")
 
-###
-    # def __init__(self, x, y):
-    #     self.x = x
-    #     self.y = y
